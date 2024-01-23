ffa0d29b752f9a608414ba0e28c9eb500aae78f5
RaphaPavaneli <rafael_pava.2012@hotmail.com>
2024-01-23 09:48:09 -0300

    23/01/2024

--- desafio_1.py
+++ desafio_1.py
@@ -1,6 +1,7 @@
 #O microblog Twitter é conhecido por limitar as postagens em 140 caracteres. Conferir se um texto vai caber em um tuíte é sua tarefa.
 
 
+
 while True:
     T = input()
     limite_caracteres = 0
@@ -17,3 +18,10 @@ print(status)
 
 if status == "TWEET":
  print(T)
+
+
+#forma simplificada
+T = input()
+
+print ("TWEET" if len(T) >= 1 and len(T) <= 140 else "MUTE")
+    
--- desafio_2.py
+++ desafio_2.py
@@ -5,6 +5,8 @@ month = int(input())
 
 months_dict = {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"Octomber", 11:"November", 12:"December"}
 
+print(months_dict[month])
+
 if month == 1:
     print(months_dict[1])
 elif month == 2:    
